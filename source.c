8a363d7fb21d256a8b63f777753e1af146a4b7cd
Maks <maks.prilutskiy@gmail.com>
2013-03-12 08:30:28 +0400

    all bugs are gone. no more warnings during compilation

--- source.c
+++ source.c
@@ -32,6 +32,7 @@ int studentCount = 0;
 void studentAdd(student * source);
 void studentShow(student * source);
 void studentDelete(student * source);
+student * customRealloc(student * source, int sizeNew);
 
 void main(void)
 {
@@ -41,7 +42,7 @@ void main(void)
         puts("Menu:");
         puts("\n0 - Exit\n1 - Add student\n2 - Remove student\n3 - Output data");
         int inpCommand = 0; fflush(stdin);
-        if (!scanf("%d",&inpCommand)) {puts("Input Error"); continue;}
+        if (!scanf_s("%d",&inpCommand)) {puts("Input Error"); continue;}
         switch(inpCommand)
         {
         default: 
@@ -64,9 +65,9 @@ void medicalInputv1 (student * source)
     while (true)
     {
         printf("Height: "); fflush(stdin);
-        if(!scanf("%lf",&source[studentCount].medical.heightWeight.height)) {puts("Input Error"); continue;}
+        if(!scanf_s("%lf",&source[studentCount].medical.heightWeight.height)) {puts("Input Error"); continue;}
         printf("Weight: "); fflush(stdin);
-        if(!scanf("%lf",&source[studentCount].medical.heightWeight.weight)) {puts("Input Error"); continue;}
+        if(!scanf_s("%lf",&source[studentCount].medical.heightWeight.weight)) {puts("Input Error"); continue;}
         break;
     }
 }
@@ -76,13 +77,13 @@ void medicalInputv2 (student * source)
     while (true)
     {
         printf("Height: "); fflush(stdin);
-        if(!scanf("%lf",&source[studentCount].medical.heightWeightPulsPressure._height)) {puts("Input Error"); continue;}
+        if(!scanf_s("%lf",&source[studentCount].medical.heightWeightPulsPressure._height)) {puts("Input Error"); continue;}
         printf("Weight: "); fflush(stdin);
-        if(!scanf("%lf",&source[studentCount].medical.heightWeightPulsPressure._weight)) {puts("Input Error"); continue;}
+        if(!scanf_s("%lf",&source[studentCount].medical.heightWeightPulsPressure._weight)) {puts("Input Error"); continue;}
         printf("Puls: "); fflush(stdin);
-        if(!scanf("%d",&source[studentCount].medical.heightWeightPulsPressure._puls)) {puts("Input Error"); continue;}
+        if(!scanf_s("%d",&source[studentCount].medical.heightWeightPulsPressure._puls)) {puts("Input Error"); continue;}
         printf("Blood pressure: ");
-        if(!scanf("%s",&source[studentCount].medical.heightWeightPulsPressure._bloodPressure)) {puts("Input Error"); continue;}
+        if(!scanf_s("%s",&source[studentCount].medical.heightWeightPulsPressure._bloodPressure)) {puts("Input Error"); continue;}
         break;
     }
 }
@@ -91,16 +92,16 @@ void studentAdd(student * source)
 {
     puts("-------------------------Personal Info------------------------------");
     printf("First name: "); fflush(stdin);
-    scanf("%s",&source[studentCount].personal.firstName);
+    scanf_s("%s",&source[studentCount].personal.firstName);
     printf("Last name: "); fflush(stdin);
-    scanf("%s",&source[studentCount].personal.lastName);
+    scanf_s("%s",&source[studentCount].personal.lastName);
     printf("Second name: "); fflush(stdin);
-    scanf("%s",&source[studentCount].personal.secondName);
+    scanf_s("%s",&source[studentCount].personal.secondName);
     puts("-------------------------Medical data------------------------------");
     while (true) 
     {
     puts("Choose input type:\n1 - Height & weight only\n2 - Height, weight, puls and blood pressure\n");
-    fflush(stdin); if (!scanf("%d", &source[studentCount].unionType)) {puts("Input Error"); continue;}
+    fflush(stdin); if (!scanf_s("%d", &source[studentCount].unionType)) {puts("Input Error"); continue;}
     if (source[studentCount].unionType!= 1 && source[studentCount].unionType!=2) {puts("Input Error"); continue;}
     printf("uniontupe value is %d\n",source[studentCount].unionType);
     break;//if we riched here - evryth is ok, proceeding
@@ -115,8 +116,7 @@ void studentAdd(student * source)
         break;
     }
     studentCount++;//indicates how many students have been added
-    source = (student*) realloc (source, studentCount+1 * sizeof(student));
-    
+    source = customRealloc(source,studentCount+1);    
 }
 
 void studentShow(student * source)
@@ -147,7 +147,7 @@ void studentDelete(student * source)
     {
     printf("Your choise: ");
     fflush(stdin);
-    if (!scanf("%d",&inp)) { puts("Input Error"); continue; }
+    if (!scanf_s("%d",&inp)) { puts("Input Error"); continue; }
     if (inp<1 || inp >=studentCount) { puts("Input Error"); continue; }
     break;
     }
@@ -156,3 +156,10 @@ void studentDelete(student * source)
         source[i]=source[i+1];
     studentCount--;
 }
+
+student * customRealloc(student * source, int sizeNew)
+{
+    student * result = (student*)calloc(sizeNew,sizeof(student));
+    result = source;
+    return result;
+} 
